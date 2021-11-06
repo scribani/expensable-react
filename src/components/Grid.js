1e5bdc554a928b10ad9a09f9755e47f524381f72
Scribani <scribani.andres@gmail.com>
2021-11-05 19:17:54 -0500

    Create Container components

---
+++ src/components/Grid.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export default styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
+`;
---
+++ src/components/MainContainer.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export default styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  min-height: 812px;
+`;
